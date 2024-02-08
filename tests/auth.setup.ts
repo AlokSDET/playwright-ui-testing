169d2af71f7c60459072f37e3e4ac7a9279dd56c
Alok Shrivastava <alok.shrivastava@happiestminds.com>
2024-02-08 11:42:24 +0530

    log print

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -5,6 +5,13 @@ import ENV from '../utils/env';
 
 const authFile = 'playwright/.auth/user.json';
 
+
+
+
+const fs = require('fs');
+
+
+
 setup('authenticate', async ({ browser }) => {
     const context = await browser.newContext();
     const page = await context.newPage();
@@ -15,5 +22,12 @@ setup('authenticate', async ({ browser }) => {
     await expect(page).toHaveTitle('Dashboard');
     // End of authentication steps.
     await page.context().storageState({ path: authFile });
+
+    let rawdata = fs.readFileSync(authFile);
+let content = JSON.parse(rawdata);
+console.log(content);
+
+
+
     console.log(JSON.stringify(authFile));
 });
